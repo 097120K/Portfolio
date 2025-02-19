d9aded5b0d8b5d8a500314ebd0e4ece361443f79
097120k <dyung444@gmail.com>
2025-02-19 13:24:21 +0900

    retouch again

--- src/myWork_web/Disney/src/js/custom.js
+++ src/myWork_web/Disney/src/js/custom.js
@@ -20,3 +20,26 @@ setInterval(() => {
     currentIndex = nextIndex;
     //currentIndex가 nextIndex의 값을 할당받음으로써 4를 못 넘게 함
 }, sliderInterval);
+
+const sliderWrap2 = document.querySelector(".slider__wrap.min");
+const sliderImg2 = sliderWrap2.querySelector(".slider__img");
+const slider2 = sliderWrap2.querySelectorAll(".slider");
+
+let currentIndex2 = 0;   //현재 보이는 이미지
+let sliderCount2 = slider2.length;     //이미지 개수
+let sliderInterva2l = 3000;  //이미지 변경 간격
+
+//슬라이드 효과
+setInterval(() => {
+    //트랜지션 설정
+    slider2[currentIndex2].style.transition = "all, 1s";
+
+    let nextIndex2 = (currentIndex + 1) % sliderCount2;
+    //전체 개수로 나누고 남은 나머지를 활용해 4(이미지 개수)를 못 넘게 함
+
+    slider2[currentIndex2].style.opacity = "0"
+    slider2[nextIndex2].style.opacity = "1";
+
+    currentIndex2 = nextIndex2;
+    //currentIndex가 nextIndex의 값을 할당받음으로써 4를 못 넘게 함
+}, sliderInterval);
