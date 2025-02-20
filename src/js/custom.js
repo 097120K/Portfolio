f7e6e763be592a209df07b9bbce56c837db24e63
097120k <dyung444@gmail.com>
2025-02-20 17:10:09 +0900

    myWork add

--- src/js/custom.js
+++ src/js/custom.js
@@ -53,6 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
 const title_div = document.querySelector('.title_div');
 const h3El = document.querySelector('h3.title');
 const subTitle = document.querySelector('p.subTitle');
+const aboutMe = document.querySelector('#aboutMe');
 const aboutMe_sub = document.querySelector('.mine_wrap');
 const aboutMe_skills = document.querySelector('.aboutMe_skills');
 const aboutMe_container = document.querySelector('.aboutMe_container');
@@ -64,6 +65,7 @@ title_div.addEventListener('click',()=>{
   subTitle.classList.toggle('show');
   aboutMe_sub.classList.toggle('show');
   aboutMe_skills.classList.toggle('show');
+  aboutMe.classList.toggle('padding');
 })
 h3El.addEventListener('click',()=>{
   title_div.classList.toggle('hide');
@@ -71,6 +73,7 @@ h3El.addEventListener('click',()=>{
   subTitle.classList.toggle('show');
   aboutMe_sub.classList.toggle('show');
   aboutMe_skills.classList.toggle('show');
+  aboutMe.classList.toggle('padding');
 })
 
 /* myWork */
@@ -118,3 +121,19 @@ document.addEventListener("DOMContentLoaded", function () {
   updateSlide();
 });
 
+const hamburger = document.querySelector('.hamburger_icon');
+const hamMenu = document.querySelector('.hamburger');
+const xBox = document.querySelector('.xBox')
+const hamLiEls = document.querySelectorAll('.hamburger .clickMenu ul li')
+
+hamburger.addEventListener("click",()=>{
+  hamMenu.classList.toggle("show")
+})
+xBox.addEventListener("click",()=>{
+  hamMenu.classList.toggle("show")
+})
+hamLiEls.forEach((hamLiEl)=>{
+  hamLiEl.addEventListener("click", ()=>{
+    hamMenu.classList.remove("show")
+  });
+});
